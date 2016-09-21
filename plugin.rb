461c21cbba68806db34394f8cd18aad556cf5930
Guo Xiang Tan <tgx_world@hotmail.com>
2016-09-21 10:51:10 +0800

    Version bump.

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-cakeday
 # about: Show a birthday cake beside the user's name on their birthday or on the date they joined Discourse.
-# version: 0.0.10
+# version: 0.0.11
 # authors: Alan Tan
 # url: https://github.com/tgxworld/discourse-cakeday
 
