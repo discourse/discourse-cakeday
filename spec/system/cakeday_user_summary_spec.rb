2716126537406bff0a7001d8763bc73d49e422ac
Yuriy Kurant <yuriy@discourse.org>
2025-06-12 17:24:07 +0800

    dev: user-cakeday: spec updates

--- spec/system/cakeday_user_summary_spec.rb
+++ spec/system/cakeday_user_summary_spec.rb
@@ -2,31 +2,31 @@
 
 describe "Cakeday/Birthday emojis", type: :system do
   CONTROL_DATE = Time.zone.local(2020, 6, 10)
-  fab!(:admin_w_birthday) { Fabricate(:admin, date_of_birth: CONTROL_DATE.prev_year(14)) }
+  fab!(:current_user) { Fabricate(:admin, date_of_birth: CONTROL_DATE.prev_year(14)) }
 
   let(:user_page) { PageObjects::Pages::User.new }
   let(:user_menu) { PageObjects::Components::UserMenu.new }
 
-  before { sign_in(admin_w_birthday) }
+  before { sign_in(current_user) }
 
-  context "for users with `created_at` and `date_of_brith` dates" do
-    fab!(:user_w_cakeday) { Fabricate(:user, created_at: CONTROL_DATE.prev_year) }
+  context "for users with `created_at` and `date_of_birth` dates" do
+    fab!(:user_with_cakeday) { Fabricate(:user, created_at: CONTROL_DATE.prev_year) }
 
     it "correctly shows emojis in users' profiles" do
       page.driver.with_playwright_page do |pw_page|
         pw_page.clock.install(time: CONTROL_DATE)
       end
 
-      user_page.visit(user_w_cakeday)
+      user_page.visit(user_with_cakeday)
 
-      expect(page).to have_current_path("/u/#{user_w_cakeday.username}/summary")
-      user_page.assert_selector(".user-cakeday div[title='Today is the anniversary of the day I joined this community!']")
+      expect(page).to have_current_path("/u/#{user_with_cakeday.username}/summary")
+      expect(user_page).to have_css(".user-cakeday div[title=\"#{I18n.t('js.user.anniversary.title')}\"] .emoji[alt='cake']")
 
       user_menu.open.click_profile_tab
       find(".summary").click
 
-      expect(page).to have_current_path("/u/#{admin_w_birthday.username}/summary")
-      user_page.assert_selector(".user-cakeday div[title='Today is your birthday!']")
+      expect(page).to have_current_path("/u/#{current_user.username}/summary")
+      expect(user_page).to have_css(".user-cakeday div[title=\"#{I18n.t('js.user.date_of_birth.user_title')}\"] .emoji[alt='birthday']")
     end
   end
 end
