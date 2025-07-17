bf94fbbaab17eede8c0d48f108175b060f784592
Jarek Radosz <jarek@cvx.dev>
2025-07-17 12:43:41 +0200

    Use the actual value

--- db/migrate/20250717093505_persist_cakeday_enabled.rb
+++ db/migrate/20250717093505_persist_cakeday_enabled.rb
@@ -3,9 +3,9 @@
 class PersistCakedayEnabled < ActiveRecord::Migration[7.2]
   def up
     # 5 is bool data_type
-    DB.exec <<~SQL
+    DB.exec(<<~SQL, value: SiteSetting.cakeday_enabled ? "t" : "f")
       INSERT INTO site_settings(name, data_type, value, created_at, updated_at)
-      VALUES('cakeday_enabled', 5, 't', NOW(), NOW())
+      VALUES('cakeday_enabled', 5, :value, NOW(), NOW())
       ON CONFLICT (name) DO NOTHING
     SQL
   end
